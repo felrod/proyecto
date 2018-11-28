047bdc118b013fb8a241dac92cae47d9c9b5bdc9
Changa Guan <changa_1994@hotmail.com>
2018-11-28 17:37:54 -0300

    Lo hecho en clase

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('template.base')
+
+@section('title',DH-Ecommerce Home)
+@endsection
+
+@section('content')
+  <h2>esta es la home</h2>
+@endsection
---
+++ resources/views/template/base.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> @yield('title')</title>
+    <link rel="stylesheet" href={{ asset('/cssbootstrap.min.css') }}>
+  </head>
+  <body>
+    @include('partials.header')
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          @yield('content')
+        </div>
+      </div>
+    </div>
+    @include('partials.footer')
+    <script src={{ asset('/js/jquery-3.3.1.min.js')}} ></script>
+		<script src={{ asset('/js/bootstrap.min.js')}} ></script>
+		@yield('custom-js')
+  </body>
+</html>
