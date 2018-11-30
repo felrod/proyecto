d74a2644ce9dd3cc27172208b14e5fed85a838c6
Changa Guan <changa_1994@hotmail.com>
2018-11-30 13:30:17 -0300

    add routes [web.php]

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ Route::get('/', function () {
 });
 
 Route::get('/home', 'PagesController@home');
+
+Route::get('/aboutus', 'PagesController@aboutUs');
+
+Route::get('/contacts', 'PagesController@contacts');
+
+Route::get('/cart', 'PagesController@cart');
+
+Route::get('/profile', 'PagesController@profile');
+
+Route::get('/editprofile', 'PagesController@editProfile');
+
+Route::get('/news', 'PagesController@news');
+
+Route::get('/offers', 'PagesController@offers');
+
+Route::get('/hotsales', 'PagesController@hotSales');
+
+Route::get('/terms&conds', 'PagesController@termsConditions');
+
+Route::get('/faq', 'PagesController@faq');
+
+Route::get('/signsucces', 'PagesController@signSuccess');
+
+Route::get('/login', 'PagesController@login');
+
+Route::get('/signup', 'PagesController@signUp');
+
+Route::get('/products', 'PagesController@products');
+
+Route::get('/productdetail', 'PagesController@productDetails');
