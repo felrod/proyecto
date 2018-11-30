5b7e65a328576207fa6c0abf564eb1a1d7c4503b
Carlosc98 <carlos_cortez-@hotmail.com>
2018-11-30 14:12:49 -0300

    migracion vistas

---
+++ resources/views/news.blade.php
@@ -0,0 +1,18 @@
+@extends('template.base')
+
+@section('title')
+@endsection
+
+@section('content')
+<?php
+// require_once("config.php");
+// if ($auth->estaLogueado()) {
+// $user = $db->traerPorMail($_SESSION["logueado"]);
+// }
+?>
+  <body>
+    <h1>NOVEDADES xddddddddd!!</h1>
+    <br><br><br>
+  </body>
+
+@endsection
---
+++ resources/views/signSuccess.blade.php
@@ -0,0 +1,38 @@
+@extends('template.base')
+
+@section('title')
+@endsection
+
+@section('content')
+<?php
+// require_once("config.php");
+// if ($auth->estaLogueado()) {
+// $user = $db->traerPorMail($_SESSION["logueado"]);
+// }
+?>
+
+<body>
+    <div class="col-md-12">
+
+      <div class="mensajesuccess">
+        <strong>Perfecto! </strong> Su registro fue satisfactorio.
+      </div>
+
+      <div class="imagensuccess">
+        <img src="images/successwhite.jpg">
+      </div>
+
+      <div class="linksinicioperfil">
+        <div class="row">
+          <div class="col-md-6">
+            <button type="button" class="btn btn-primary ml-auto login" data-toggle="modal" data-target="#myModal"><a href="index.php" style="color:white;">Inicio</a></button>
+          </div>
+          <div class="col-md-6">
+            <button type="button" class="btn btn-primary ml-auto login" data-toggle="modal" data-target="#myModal"><a href="profile.php" style="color:white;">Perfil</a></button>
+          </div>
+        </div>
+      </div>
+
+    </div>
+</body>
+@endsection
