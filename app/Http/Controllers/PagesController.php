26287a0261c33a8a0daf7589a1459124db8271fa
Changa Guan <changa_1994@hotmail.com>
2018-12-18 14:27:25 -0300

    add[categories.blades]

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -65,4 +65,49 @@ class PagesController extends Controller
       return view('signUp');
     }
 
+    public function catCelulares(){
+      $products = \App\Product::where('category_id', 1)->get();
+      return view('categories.celulares')->with(compact('products'));
+    }
+
+    public function catAire(){
+      $products = \App\Product::where('category_id', 2)->get();
+      return view('categories.airesycalefaccion')->with(compact('products'));
+    }
+
+    public function catCamaras(){
+      $products = \App\Product::where('category_id', 3)->get();
+      return view('categories.celulares')->with(compact('products'));
+    }
+
+    public function catCocina(){
+      $products = \App\Product::where('category_id', 4)->get();
+      return view('categories.cocina')->with(compact('products'));
+    }
+
+    public function catOficina(){
+      $products = \App\Product::where('category_id', 5)->get();
+      return view('categories.oficina')->with(compact('products'));
+    }
+
+    public function catInformatica(){
+      $products = \App\Product::where('category_id', 6)->get();
+      return view('categories.informatica')->with(compact('products'));
+    }
+
+    public function catLavado(){
+      $products = \App\Product::where('category_id', 7)->get();
+      return view('categories.lavado')->with(compact('products'));
+    }
+
+    public function catGaming(){
+      $products = \App\Product::where('category_id', 8)->get();
+      return view('categories.gaming')->with(compact('products'));
+    }
+
+    public function catTv(){
+      $products = \App\Product::where('category_id', 9)->get();
+      return view('categories.tv')->with(compact('products'));
+    }
+
 }
