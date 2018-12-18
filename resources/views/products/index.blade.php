a196d8fce42c52a7583e640592a0a0c1841fd9f2
Changa Guan <changa_1994@hotmail.com>
2018-12-18 16:51:39 -0300

    prueba

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -18,7 +18,9 @@
         <div class="col-lg-3 col-md-6 mb-4">
           <div class="card">
             <img class="card-img-top" src="{{ Storage::url('products/' . $oneProduct->img) }}" alt="image" style="width:100%">
-            <img src="/images/offer.png" alt="" style="position:absolute; top:0 ; left:0; width:70px;">
+            @if ($oneProduct->status_id=2)
+              <img src="/images/offer.png" alt="" style="position:absolute; top:0 ; left:0; width:70px;">
+            @endif
             <div class="card-body">
               <h4 class="card-title">$ {{ $oneProduct->price }}</h4>
               <p class="card-text">{{ $oneProduct->name }}</p>
